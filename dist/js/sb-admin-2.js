d29951a5181b491a4e11f803283ab3b3c7dc9444
wumingdan <creditmail@126.com>
2016-04-21 18:01:45 +0800

    add tools

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -34,3 +34,23 @@ $(function() {
         element.addClass('active');
     }
 });
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?eb8e497b3383518a5c42758a16b4007f";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1258731748'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1258731748' type='text/javascript'%3E%3C/script%3E"));
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-67825764-3', 'auto');
+ga('send', 'pageview');
