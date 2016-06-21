7e2733081a67b166d240aa9bf361aa38e38274bf
hugeloli <limengge916@163.com>
2016-06-21 16:34:53 +0800

    Update ga-events.js

--- dist/js/ga-events.js
+++ dist/js/ga-events.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
     // panel 1
-    $('#panel-1').on('clicn', function () {
+    $('#panel-1').on('click', function () {
         ga('send', 'event', 'open link in current window', 'click', 'New Comments', 'open link in current window via click `New Comments`');
         window.location.href = 'http://www.hugeloli.com/third/2048/';
     });
 
     // panel 2
-    $('#panel-2').on('clicn', function () {
+    $('#panel-2').on('click', function () {
         ga('send', 'event', 'open link in new tab', 'click', 'open link in new tab', 'open link in new tab via click `open link in new tab`');
         window.open('http://www.hugeloli.com/third/fireworks/');
     });
 
     // panel 3
-    $('#panel-3').on('clicn', function () {
+    $('#panel-3').on('click', function () {
         ga('send', 'event', 'open link in current window', 'double-click', 'New Orders', 'open link in current window via double-click `New Orders`');
         window.location.href = 'http://www.hugeloli.com/third/2048/';
     });
 
     // panel 4
-    $('#panel-4').on('clicn', function () {
+    $('#panel-4').on('click', function () {
         ga('send', 'event', 'open link in new tab', 'double-click');
         window.open('http://www.hugeloli.com/third/fireworks/');
     });
