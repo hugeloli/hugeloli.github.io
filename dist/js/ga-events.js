6d29a9cbb66f6a904a63ccebbc94e0237311ce7d
wumingdan <wumingdan@baidu.com>
2016-06-21 16:29:48 +0800

    push

--- dist/js/ga-events.js
+++ dist/js/ga-events.js
@@ -1,1 +1,25 @@
-console.log('hello world')
+$(document).ready(function () {
+    // panel 1
+    $('#panel-1').on('clicn', function () {
+        ga('send', 'event', 'open link in current window', 'click', 'New Comments', 'open link in current window via click `New Comments`');
+        window.location.href = 'http://www.hugeloli.com/third/2048/';
+    });
+
+    // panel 2
+    $('#panel-2').on('clicn', function () {
+        ga('send', 'event', 'open link in new tab', 'click', 'open link in new tab', 'open link in new tab via click `open link in new tab`');
+        window.open('http://www.hugeloli.com/third/fireworks/');
+    });
+
+    // panel 3
+    $('#panel-3').on('clicn', function () {
+        ga('send', 'event', 'open link in current window', 'double-click', 'New Orders', 'open link in current window via double-click `New Orders`');
+        window.location.href = 'http://www.hugeloli.com/third/2048/';
+    });
+
+    // panel 4
+    $('#panel-4').on('clicn', function () {
+        ga('send', 'event', 'open link in new tab', 'double-click');
+        window.open('http://www.hugeloli.com/third/fireworks/');
+    });
+});
