3fd65ecef7070f2df461e6346d1143b451e42f38
hugeloli <limengge916@163.com>
2016-09-20 13:40:24 +0800

    Update sb-admin-2.js

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -45,7 +45,7 @@ var _hmt = _hmt || [];
 })();
 */
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F779249f676ef4ead987e8c142b9984b7' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='" + " https://" + "hm.baidu.com/h.js%3F779249f676ef4ead987e8c142b9984b7' type='text/javascript'%3E%3C/script%3E"));
 
 var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1258731748'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1258731748' type='text/javascript'%3E%3C/script%3E"));
