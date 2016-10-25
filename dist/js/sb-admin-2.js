106ab1ac5b27b0378303680eca2c67923aa23007
hugeloli <limengge916@163.com>
2016-10-25 16:57:24 +0800

    Update sb-admin-2.js

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -59,3 +59,19 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-67825764-3', 'auto');
 ga('send', 'pageview');
 
+// ptengine tracking
+(function () {
+    window._pt_lt = new Date().getTime();
+    window._pt_sp_2 = [];
+    _pt_sp_2.push('setAccount,2a6d7de4');
+    var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    (function() {
+        var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+        atag.src = _protocol + 'js.ptengine.cn/2a6d7de4.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(atag, s);
+    })();
+})();
+
+
+
