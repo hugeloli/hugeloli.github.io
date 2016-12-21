3ab6b9f504377110e5f938aedab7fcf75281c0e2
hugeloli <limengge916@163.com>
2016-12-21 18:39:38 +0800

    Update sb-admin-2.js

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -73,5 +73,18 @@ ga('send', 'pageview');
     })();
 })();
 
-
+// gio
+var _vds = _vds || [];
+window._vds = _vds;
+(function(){
+    _vds.push(['setAccountId', 'a52c6f971674fed7']);
+    (function() {
+      var vds = document.createElement('script');
+      vds.type='text/javascript';
+      vds.async = true;
+      vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(vds, s);
+    })();
+})();
 
